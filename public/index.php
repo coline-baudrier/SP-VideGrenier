f4a2e6b3ad7ff9230b6ed0495ec946430109f4b1
coline_baudrier <coline.baudrier@outlook.com>
2024-04-22 19:15:47 +0200

    Modification index.php pour chemin et class not found

--- public/index.php
+++ public/index.php
@@ -14,9 +14,11 @@ session_start();
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 
+
 /**
  * Error and Exception handling
  */
+require __DIR__ . '../../Core/Error.php';
 error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
