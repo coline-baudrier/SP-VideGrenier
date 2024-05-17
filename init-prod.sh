56b3b7ca312cc4f871d36bb62280f85bf0ac4c4e
coline_baudrier <coline.baudrier@outlook.com>
2024-05-17 15:20:15 +0200

    Modification init-prod.sh

--- init-prod.sh
+++ init-prod.sh
@@ -9,15 +9,15 @@ TEMP_DIR="/tmp/repo"
 # Nettoyer le répertoire de destination s'il existe et n'est pas vide
 if [ -d "$DEST_DIR" ] && [ "$(ls -A $DEST_DIR)" ]; then
   echo "Nettoyage du répertoire $DEST_DIR..."
-  rm -rf ${DEST_DIR:?}/*
+  rm -rf "${DEST_DIR:?}"/*  # Utilisation de guillemets pour gérer les espaces potentiels dans les chemins
 fi
 
 # Nettoyer le répertoire temporaire s'il existe
 if [ -d "$TEMP_DIR" ]; then
-  rm -rf ${TEMP_DIR}
+  rm -rf "${TEMP_DIR}"  # Utilisation de guillemets ici aussi
 fi
 
-# Clone the repository dans le répertoire temporaire
+# Cloner le dépôt dans le répertoire temporaire
 git clone --single-branch --branch $BRANCH $REPO_URL $TEMP_DIR
 
 # Vérifier si index.php existe dans le répertoire public
@@ -27,10 +27,11 @@ if [ ! -f "$TEMP_DIR/public/index.php" ]; then
 fi
 
 # Copier le contenu du dossier public vers le répertoire de destination
-cp -r $TEMP_DIR/public/* $DEST_DIR
+cp -r "$TEMP_DIR/public/"* "$DEST_DIR"  # Ajout de guillemets pour sécuriser les chemins
 
 # Option : installer les dépendances, etc.
-#cd $TEMP_DIR
+# Les commandes suivantes sont commentées, décommentez-les si nécessaire
+#cd "$TEMP_DIR"  # Ajout de guillemets
 # composer install
 # php artisan migrate
 
